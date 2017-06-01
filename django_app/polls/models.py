e4e7a0592429bcf31bde8896238fd5abf9096c32
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-01 12:59:15 +0900

    add polls application to INSTALLED_APPS, create Question, Choice models, make DB migtations

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField('질문내용', max_length = 200)
+    pub_date = models.DateTimeField('발행일자')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, verbose_name = '해당 질문', on_delete = models.CASCADE)
+    choice_text = models.CharField('선택내용', max_length = 200)
+    votes = models.IntegerField('총 투표수', default = 0)
